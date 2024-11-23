3d070b4ca156586c8217c14e3e4beec431146a32
Dan Sapp <dan.sapp@storable.com>
2024-11-22 19:39:26 -0600

    refetch on toggle or delete

--- lib/src/device_manager/home.dart
+++ lib/src/device_manager/home.dart
@@ -1,10 +1,11 @@
 import '../settings/settings_view.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import 'models/device.dart';
 import 'bottom_nav_bar.dart';
 import 'device_tile.dart';
-import 'api.dart';
+// import 'api.dart';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -16,13 +17,87 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  bool _isLoading = true;
   late Future<List<Device>> listDevices;
+  List<Device> _devices = [];
   int currentPageIndex = 0;
   @override
   void initState() {
     super.initState();
     listDevices = fetchDevices();
-    // createDevice();
+  }
+
+  Future<List<Device>> fetchDevices() async {
+    var url =
+        Uri.parse('https://bastion-server-951fbdb64d29.herokuapp.com/devices');
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        var resData = json.decode(response.body)["data"]["items"] as List;
+        var devices = resData.map((i) => Device.fromJSON(i)).toList();
+
+        // Use setState to update the devices list and loading status
+        setState(() {
+          _devices = devices;
+          _isLoading = false; // Hide loading indicator after fetching
+        });
+        return devices;
+      } else {
+        throw Exception('Failed to load devices');
+      }
+    } catch (error) {
+      setState(() {
+        _isLoading = false;
+      });
+      return [];
+      // Handle the error (maybe show a message to the user)
+      // print('Error fetching devices: $error');
+    }
+  }
+
+  Future<bool> deleteDevice(int id) async {
+    // var url = Uri.parse('http://localhost:8080/devices');
+    var url = Uri.parse(
+        'https://bastion-server-951fbdb64d29.herokuapp.com/devices/${id}');
+
+    final response = await http.delete(url, headers: {
+      'Content-Type': 'application/json',
+    });
+
+    if (response.statusCode == 200) {
+      fetchDevices();
+      return true;
+    } else {
+      throw Exception('Failed to create devices');
+    }
+  }
+
+  Future<bool> toggleDevice(Device device, bool toOn) async {
+    var url = Uri.parse(
+        'https://bastion-server-951fbdb64d29.herokuapp.com/devices/${device.id}');
+    var updatedDevice = {
+      "id": device.id,
+      "name": device.name,
+      "type": device.type,
+      "isOn": toOn,
+    };
+    var payload = json.encode(updatedDevice);
+
+    final response = await http.put(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: payload,
+    );
+
+    if (response.statusCode == 200) {
+      fetchDevices();
+      return true;
+    } else {
+      throw Exception('Failed to toggle device state: ${response.body}');
+    }
   }
 
   Widget _buildBody() {
@@ -32,7 +107,7 @@ class HomeState extends State<Home> {
           future: listDevices,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              var devices = snapshot.data as List<Device>;
+              // var devices = snapshot.data as List<Device>;
               return GridView.builder(
                 padding: const EdgeInsets.all(10),
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -41,9 +116,9 @@ class HomeState extends State<Home> {
                   mainAxisSpacing: 15,
                   childAspectRatio: 1, // Adjust based on the design
                 ),
-                itemCount: devices.length,
+                itemCount: _devices.length,
                 itemBuilder: (context, index) {
-                  var device = devices[index];
+                  var device = _devices[index];
                   return DeviceTile(
                     device: device,
                     onTurnOn: () => toggleDevice(device, true),
@@ -118,7 +193,9 @@ class HomeState extends State<Home> {
             ),
           ],
         ),
-        body: _buildBody(),
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator()) // Loading spinner
+            : _buildBody(),
         bottomNavigationBar: BottomNavBar(
           currentIndex: currentPageIndex,
           onItemSelected: (int index) {
