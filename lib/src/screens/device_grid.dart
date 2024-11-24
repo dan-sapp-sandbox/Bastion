de063586f68b10a75caba4b949d4c8f2e492f741
Dan Sapp <dan.sapp@storable.com>
2024-11-23 21:47:13 -0600

    try 2 columns

--- lib/src/screens/device_grid.dart
+++ lib/src/screens/device_grid.dart
@@ -14,9 +14,9 @@ class DeviceGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 15,
         childAspectRatio: 1,
       ),
       itemCount: devices.length,
